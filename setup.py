ded6cb1fd1c88ac4fc40aa1abdc1bd2e562a9479
Gemma Hentsch <gemma@halfapenguin.com>
2014-08-30 13:54:15 +0100

    scaffolding

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='Presser',
+    version='0.1dev',
+    packages=['presser',],
+    license='GNU GPL v3.0',
+    long_description=open('README.txt').read(),
+)
