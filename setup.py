0309344fe3b4bed814afd468e3467ec95ef81398
Gemma Hentsch <gemma@halfapenguin.com>
2014-08-30 17:17:35 +0100

    setup file tweaks

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from distutils.core import setup
 
-setup(name='Presser',
-    version='0.1dev',
-    packages=['presser',],
-    license='GNU GPL v3.0',
-    long_description=open('README.txt').read(),
+setup(name="Presser",
+    version="0.1",
+    packages=["presser",],
+    license="GNU GPL v3.0",
+    description="Extracts data from vine, in lieu of an API",
+    author="Gemma Hentsch",
+    author_email="contact@halfapenguin.com",
+    requires=["beautifulsoup4(>=4.3.2)","requests(>=2.4.0)"],
+    long_description=open("README.txt").read(),
 )
